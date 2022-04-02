244bb2b732156651a49184d230f4d347709c724e
FTEPXW@stud.uni-obuda.hu <FTEPXW@stud.uni-obuda.hu>
2022-04-02 22:41:43 +0200

    background diplay test

--- GUI_20212202_AYZ8R9/Renderer/Display.cs
+++ GUI_20212202_AYZ8R9/Renderer/Display.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using GUI_20212202_AYZ8R9.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GUI_20212202_AYZ8R9.Renderer
 {
-    internal class Display
+    public class Display : FrameworkElement
     {
+        IGameModel model;
+
+        public void SetupModel(IGameModel model)
+        {
+            this.model = model;
+        }
+
+
     }
 }
